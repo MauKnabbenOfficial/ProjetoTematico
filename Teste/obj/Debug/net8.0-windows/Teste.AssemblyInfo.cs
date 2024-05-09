fd53fa35ff0a6c938ce4e23a2173763c07c9e144
Mauricio Knabben <91981855+MauKnabbenOfficial@users.noreply.github.com>
2024-05-08 22:31:58 -0300

    teste

--- Teste/obj/Debug/net8.0-windows/Teste.AssemblyInfo.cs
+++ Teste/obj/Debug/net8.0-windows/Teste.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+088bb59cad0f2fe88f2999c760498f2669554f57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42ef0fa0fff39d534556657204749171a9655dbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
